038542ff70f8aca9624529104f92410bc629fd99
LeoSipowicz <leojsipo@gmail.com>
2022-06-20 12:58:05 -0600

    importing size and shape of objects from rapier

--- main.js
+++ main.js
@@ -51,28 +51,87 @@ export default function run_simulation() {
     }
     
 
-    const renderer = new PIXI.Renderer();
-    const scene = new PIXI.Container();
+    //PIXI GRAPHICS
 
-    function render(world){
+    function addCollider(RAPIER, world, collider) {
+        let instanceCount = 0;
+        let type = "UNKNOWN"
+        let rad = 0
+        let sizeX = 0
+        let sizeY = 0
+
+        switch (collider.shapeType()) {
+            case RAPIER.ShapeType.Cuboid:
+                type = "CUBE"
+                let hext = collider.halfExtents();
+                sizeX = hext.x;
+                sizeY = hext.y;
+                instanceCount += 1;
+                break;
+            case RAPIER.ShapeType.Ball:
+                type = "BALL"
+                rad = collider.radius();
+                instanceCount += 1;
+                break;
+            default:
+                console.log("Unknown shape to render.");
+                break;
+        }
+        let t = collider.translation();
+        let r = collider.rotation();
+
+        const shape = {};
+        shape.type = type;
+        shape.xLoc = t.x;
+        shape.yLoc = -t.y;
+        shape.rotation = -r.angle;
+        shape.rSize = rad;
+        shape.xSize = sizeX;
+        shape.ySize = sizeY;
+
+        gfx.set(collider.handle,shape)
+    }
+    
+
+    function initShapes(world,gfx){
+        const container = new PIXI.Container();
+        app.stage.addChild(container);
         var objects = new PIXI.Graphics();
-        //objects.clear();
-        world.forEachCollider((elt)=>{
-            let translation = elt.translation();
-            //let rotation = elt.rotation();
-            objects.beginFill(0xff0000);
-            objects.drawRect(translation.x+200, -translation.y+100, 1, 1);
+        //bjects.clear();
+
+        gfx.forEach((gfx)=>{
+            if (gfx.type == "BALL"){
+                objects.beginFill(0x0000ff)
+                shape.drawCircle(gfx.xLoc,gfx.yLoc,gfx.rSize)
+            }
+            else if (gfx.type == "CUBE"){
+                //console.log(gfx.sizeY)
+                objects.beginFill(0xff0000);
+                //objects.drawRect(100,100,5,5);
+                objects.drawRect(gfx.xLoc,-gfx.yLoc,1,1);
+            }
+            container.addChild(objects);
         })
 
         app.stage.addChild(objects);
-        renderer.render(scene);
+        renderer.render(container);
     }
-    
+
+
+    const renderer = new PIXI.Renderer();
+
+    const gfx = new Map();
+
+    world.forEachCollider(coll => {
+        addCollider(RAPIER, world, coll);
+    });
+
+    initShapes(world,gfx);
+
     let gameLoop = () => {
         console.log("in gameloop")
         // Ste the simulation forward.  
         world.step();
-        render(world);
     
         setTimeout(gameLoop, 16);
     };
